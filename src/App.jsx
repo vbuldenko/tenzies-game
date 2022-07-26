aecad855cb22f58d20aab32eb28fa3a777851262
Vlad Buldenko <vladbuldenko@gmail.com>
2022-07-26 16:56:40 +0300

    Array added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import Die from './components/Die.jsx'
 
-function App() {
+export default function App() {
+
+    const[numbers, setNumbers] = React.useState(allNewDice)
+
+    function allNewDice(){
+        const randomArray = [];
+        for(let i = 0; i < 10; i++){
+            randomArray.push(Math.ceil(Math.random() * 6))
+        }
+        return randomArray
+    }
+    console.log(numbers)
+
+    const dies = numbers.map((num, index) => <Die key={index + 1} value={num} />)
+
     return(
         <main className="container">
-
+            <div className="grid-container">   
+                {dies}
+            </div>
+            <button className="roll-dice" onClick={() => setNumbers(allNewDice)}>Roll</button>
         </main>
     )
 }
-
-export default App
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Die(props){
+    return(
+        <div className="grid-item"><p>{props.value}</p></div>
+    )
+}
